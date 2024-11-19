e8eeb84a46e6015de7ba1a9eb68c173d4b280720
Clarisse Otmani <clarisseotm@gmail.com>
2024-11-19 12:07:31 +0100

    modification NavBar and TopBar

---
+++ gestion-iti/src/components/Nav/NavBar.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './NavBar.css'
+import { Button } from "@/components/ui/button"
+
+const NavBar = () => {
+
+    return(
+        <>
+            <Button>Home</Button>
+            <Button>Map</Button>
+            <Button>Premium</Button>
+        </>
+
+    )
+}
+export default NavBar
--- gestion-iti/src/components/Nav/TopBar.jsx
+++ gestion-iti/src/components/Nav/TopBar.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import Avatar from '../Div/UserAvatar.jsx';
+import Badges from '../Div/Badges.jsx'
 import { Switch } from "@/components/ui/switch"// Import du composant UserAvatar
 import './TopBar.css';
 
@@ -7,7 +8,9 @@ const TopBar = () => {
     return (
         <div className="top-bar">
             <h1 className="top-bar-title">Bienvenue</h1>
+
             <div className="top-bar-right">
+                <Badges/>
                 <Switch></Switch>
                 <Avatar/> {/* Utilisation de UserAvatar */}
             </div>
