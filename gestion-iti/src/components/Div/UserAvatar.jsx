5c3a134c92c36951bcaeaad3a0cb974dae36c120
Clarisse Otmani <clarisseotm@gmail.com>
2024-11-19 11:34:56 +0100

    creation Badge.jsx and UserAvatar.jsx

---
+++ gestion-iti/src/components/Div/UserAvatar.jsx
@@ -0,0 +1,9 @@
+// ./components/UserAvatar.jsx
+import React from 'react';
+import {Avatar} from "../ui/avatar.jsx"; // Ton composant Avatar
+
+const UserAvatar = () => {
+    return <Avatar name="Segun Adebayo" src="https://bit.ly/sage-adebayo" />;
+};
+
+export default UserAvatar;
