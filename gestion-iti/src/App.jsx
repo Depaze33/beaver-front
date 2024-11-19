bfab1d3288df57a02125942fde8a0e7a8d418ddc
Clarisse Otmani <clarisseotm@gmail.com>
2024-11-19 12:08:19 +0100

    add FilterBar.jsx

--- gestion-iti/src/App.jsx
+++ gestion-iti/src/App.jsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import Map from './components/Map';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import TopBar from "@/components/Nav/TopBar.jsx";
+import NavBar from "@/components/Nav/NavBar.jsx";
 import './App.css';
 
 
@@ -9,6 +10,7 @@ function App() {
     return (
         <BrowserRouter>
             <TopBar/>
+            <NavBar/>
             <Routes>
                 <Route>
                     <Route path='/map' element={<Map />} />
---
+++ gestion-iti/src/components/Div/Badges.jsx
@@ -0,0 +1,11 @@
+import { Badge, Stack } from "@chakra-ui/react"
+
+const Badges = () => {
+    return (
+        <Stack direction="row">
+            <Badge colorPalette="green">Success</Badge>
+
+        </Stack>
+    )
+}
+export default Badges
