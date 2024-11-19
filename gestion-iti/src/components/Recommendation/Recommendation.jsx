275ed519671a96e905dbe9e856d219598d730115
Clarisse Otmani <clarisseotm@gmail.com>
2024-11-19 15:06:21 +0100

    SearchBar + Recommendation creation

--- gestion-iti/src/components/Recommendation/Recommendation.jsx
+++ gestion-iti/src/components/Recommendation/Recommendation.jsx
@@ -1,8 +1,12 @@
 import React from 'react'
 import Filter from './Filter/FilterBar.jsx'
+import RecommendationList from "@/components/Recommendation/RecommendationList/RecommendationList.jsx";
 const Recommendation = () => {
     return (
-        <div><Filter/></div>
+        <div><Filter/>
+        <RecommendationList/>
+        </div>
+
     )
 }
 export default Recommendation
---
+++ gestion-iti/src/components/Recommendation/RecommendationList/RecommendationList.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import RecoCard from "@/components/Recommendation/RecommendationList/RecoCard.jsx";
+
+const RecommendationList = () => {
+    return (
+        <>
+        <RecoCard/>
+        <RecoCard/>
+        </>
+    )
+}
+export default RecommendationList
