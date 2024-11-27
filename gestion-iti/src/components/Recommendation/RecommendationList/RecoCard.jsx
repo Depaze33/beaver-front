4a5e1625321de562571569be857014b70ed41122
Clarisse Otmani <clarisseotm@gmail.com>
2024-11-27 15:10:00 +0100

    add style for recommandation

--- gestion-iti/src/components/Recommendation/RecommendationList/RecoCard.jsx
+++ gestion-iti/src/components/Recommendation/RecommendationList/RecoCard.jsx
@@ -1,5 +1,3 @@
-import {Card} from "@chakra-ui/react"
-import {Avatar} from "@/components/ui/avatar"
 import {Button} from "@/components/ui/button"
 import PropTypes from "prop-types";
 import './RecoCard.css'
@@ -18,7 +16,7 @@ const RecoCard = ({img, type, title, distance, comment}) => {
                     <div className="card-type">
                         <h3>{type}</h3>
                     </div>
-                    <div className="card-body">
+                    <div className="card-element">
                         <div className="card-title">
                             <h3>{title}</h3>
                         </div>
@@ -26,27 +24,27 @@ const RecoCard = ({img, type, title, distance, comment}) => {
                         <div className="card-comment">
                             <p>{comment}</p>
                         </div>
-                        <div className="card-distance">
-
-                            <span>Distance: {distance} m</span>
-                        </div>
                     </div>
-                </div>
 
-                <div className="card-footer" style={{display: 'flex', justifyContent: 'flex-end'}}>
-                    <Button variant="outline">👍</Button>
-                    {/*<Button>👎</Button>*/}
+                </div>
+                <div className="card-distance">
+
+                    <span>Distance: {distance} m</span>
                 </div>
             </div>
+            <div className="card-footer" style={{display: 'flex', justifyContent: 'flex-end'}}>
+                <Button variant="outline">👍</Button>
+                {/*<Button>👎</Button>*/}
             </div>
-            )
-            }
-            RecoCard.propTypes = {
-            img: PropTypes.string,
-            type: PropTypes.string,
-            title: PropTypes.string,
-            distance: PropTypes.number,
-            comment: PropTypes.string
-        };
+        </div>
+    )
+}
+RecoCard.propTypes = {
+    img: PropTypes.string,
+    type: PropTypes.string,
+    title: PropTypes.string,
+    distance: PropTypes.number,
+    comment: PropTypes.string
+};
 
-            export default RecoCard
+export default RecoCard
