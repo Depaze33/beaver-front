1f3e04ddd32fa1709ca4c8893e74ac3d1b927023
Clarisse Otmani <clarisseotm@gmail.com>
2024-12-11 14:52:57 +0100

    make css

--- gestion-iti/src/main.jsx
+++ gestion-iti/src/main.jsx
@@ -1,9 +1,8 @@
 import { Provider } from "@/components/ui/provider"
-import React from "react"
 import ReactDOM from "react-dom/client"
-import { ChakraProvider } from "@chakra-ui/react";
+//import { extendTheme } from "@chakra-ui/react";
 import App from "./App"
-// import { ColorModeScript } from '@chakra-ui/react'
+//import { ColorModeScript } from '@chakra-ui/react'
 // import theme from './theme'
 
 ReactDOM.createRoot(document.getElementById("root")).render(
