2e7d717d1cbdde673f99066ffa0de92b2c7e057a
Clarisse Otmani <clarisseotm@gmail.com>
2024-12-12 08:44:49 +0100

    modification icon navbar

--- gestion-iti/src/components/Nav/NavBar.jsx
+++ gestion-iti/src/components/Nav/NavBar.jsx
@@ -1,20 +1,33 @@
-import React from "react";
-import { Link} from "@chakra-ui/react";
+import {Link} from "@chakra-ui/react";
 import "./NavBar.css";
-import { FaHome } from "react-icons/fa";
-import { FaMapMarkedAlt } from "react-icons/fa";
-import { IoIosContacts } from "react-icons/io";
-import { FaCrown } from "react-icons/fa6";
+import {FaHome} from "react-icons/fa";
+import {FaMapMarkedAlt} from "react-icons/fa";
+import {IoIosContacts} from "react-icons/io";
+import {FaCrown} from "react-icons/fa6";
 
 const NavBar = () => {
-
-
     return (
-        <div className={`nav-bar nav-bar`}>
-            <Link className="icon" href="/"><span className="nav-icon"><FaHome /></span></Link>
-            <Link className="icon" href="/map"><span className="nav-icon"><FaMapMarkedAlt /></span></Link>
-            <Link className="icon" href="/contact"><span className="nav-icon"><IoIosContacts /></span></Link>
-            <Link className="icon" href="/premium"><span className="nav-icon"><FaCrown /></span></Link>
+        <div className="nav-bar">
+            <Link href="/" passHref>
+                <a className="icon" aria-label="Home">
+                    <span className="nav-icon"><FaHome size="24" /></span>
+                </a>
+            </Link>
+            <Link href="/map" passHref>
+                <a className="icon" aria-label="Map">
+                    <span className="nav-icon"><FaMapMarkedAlt size="24" /></span>
+                </a>
+            </Link>
+            <Link href="/contact" passHref>
+                <a className="icon" aria-label="Contact">
+                    <span className="nav-icon"><IoIosContacts size="24" /></span>
+                </a>
+            </Link>
+            <Link href="/premium" passHref>
+                <a className="icon" aria-label="Premium">
+                    <span className="nav-icon"><FaCrown size="24" /></span>
+                </a>
+            </Link>
         </div>
     );
 };
